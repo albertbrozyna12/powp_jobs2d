4be8ccfcac20de53ae27d56338852ff89d1c1277
Albert Brozyna <245779@edu.p.lodz.pl>
2025-03-19 23:42:11 +0100

    Removed unused imports and added a new clas Job2dDriverAbstractAdapter.

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Job2dDriverAbstractAdapter.java
@@ -0,0 +1,20 @@
+package edu.kis.powp.jobs2d.drivers.adapter;
+
+import edu.kis.powp.jobs2d.AbstractDriver;
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class Job2dDriverAbstractAdapter extends AbstractDriver {
+    private final Job2dDriver job2dDriver;
+
+    Job2dDriverAbstractAdapter(Job2dDriver job2dDriver){
+        super(0,0);
+        this.job2dDriver = job2dDriver;
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+        job2dDriver.setPosition(getX(),getY());
+        job2dDriver.operateTo(getX(),getY());
+        job2dDriver.setPosition(x,y);
+    }
+}
