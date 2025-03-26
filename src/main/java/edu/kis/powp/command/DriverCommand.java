42872dd0761a1069e9f29506ec3746c5af66ba05
Albert Brozyna <245779@edu.p.lodz.pl>
2025-03-26 18:33:27 +0100

    Fix DriveCommand removed public and abstract.

--- src/main/java/edu/kis/powp/command/DriverCommand.java
+++ src/main/java/edu/kis/powp/command/DriverCommand.java
@@ -3,5 +3,5 @@ package edu.kis.powp.command;
 import edu.kis.powp.jobs2d.Job2dDriver;
 
 public interface DriverCommand {
-    public abstract void execute(Job2dDriver driver);
+    void execute(Job2dDriver driver);
 }
