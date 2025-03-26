31b450f8c2c50c49a9c4ec52d503fb272f9191fd
Albert Brozyna <245779@edu.p.lodz.pl>
2025-03-26 18:32:33 +0100

    Creating a test for ComplexCommand class.

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -2,7 +2,10 @@ package edu.kis.powp.jobs2d.events;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
+import edu.kis.powp.command.ComplexCommand;
 import edu.kis.powp.command.DriverCommand;
 import edu.kis.powp.command.OperateToCommand;
 import edu.kis.powp.command.SetPositionCommand;
@@ -15,35 +18,31 @@ import edu.kis.powp.jobs2d.Figures;
 
 public class SelectTestFigureOptionListener implements ActionListener {
 
-	private DriverManager driverManager;
+    private DriverManager driverManager;
 
-	public SelectTestFigureOptionListener(DriverManager driverManager) {
-		this.driverManager = driverManager;
-	}
+    public SelectTestFigureOptionListener(DriverManager driverManager) {
+        this.driverManager = driverManager;
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
-		if(e.getActionCommand().equals(Figures.FIGURE_JOE_1.getCommand())){
-			FiguresJoe.figureScript1(driverManager.getCurrentDriver());
-		}else if(e.getActionCommand().equals(Figures.FIGURE_JOE_2.getCommand())){
-			FiguresJoe.figureScript2(driverManager.getCurrentDriver());
-		}
-		else if(e.getActionCommand().equals(Figures.FIGURE_JANE.getCommand())){
-			FiguresJane.figureScript(new Job2dDriverAbstractAdapter(driverManager.getCurrentDriver()));
-		}else if(e.getActionCommand().equals(Figures.FIGURE_CUSTOM.getCommand())){
-			DriverCommand driverCommand = new SetPositionCommand(30,30);
-			DriverCommand driverCommand1 = new OperateToCommand(-30,30);
-			DriverCommand driverCommand2 = new OperateToCommand(-30,-30);
-			DriverCommand driverCommand3 = new OperateToCommand(30,-30);
-			DriverCommand driverCommand4 = new OperateToCommand(30,30);
+        if (e.getActionCommand().equals(Figures.FIGURE_JOE_1.getCommand())) {
+            FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+        } else if (e.getActionCommand().equals(Figures.FIGURE_JOE_2.getCommand())) {
+            FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+        } else if (e.getActionCommand().equals(Figures.FIGURE_JANE.getCommand())) {
+            FiguresJane.figureScript(new Job2dDriverAbstractAdapter(driverManager.getCurrentDriver()));
+        } else if (e.getActionCommand().equals(Figures.FIGURE_CUSTOM.getCommand())) {
+            ComplexCommand complexCommand = new ComplexCommand();
 
-			driverCommand.execute(driverManager.getCurrentDriver());
-			driverCommand1.execute(driverManager.getCurrentDriver());
-			driverCommand2.execute(driverManager.getCurrentDriver());
-			driverCommand3.execute(driverManager.getCurrentDriver());
-			driverCommand4.execute(driverManager.getCurrentDriver());
-		}
-	}
+            complexCommand.addCommand(new SetPositionCommand(30, 30));
+            complexCommand.addCommand(new OperateToCommand(-30, 30));
+            complexCommand.addCommand(new OperateToCommand(-30, -30));
+            complexCommand.addCommand(new OperateToCommand(30, -30));
+            complexCommand.addCommand(new OperateToCommand(30, 30));
+            complexCommand.execute(driverManager.getCurrentDriver());
+        }
+    }
 }
 
